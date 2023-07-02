b64372dd605aa95990e9bffce27251e203bafe17
suhyeon <suhyeon3484@naver.com>
2023-07-03 01:31:26 +0900

    chore: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MuiToDoPage from './pages/MuiToDoPage';
+import StyledToDoPage from './pages/StyledToDoPage';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<MuiToDoPage />} />
+        <Route path='/main' element={<StyledToDoPage />} />
       </Routes>
     </Router>
   );
