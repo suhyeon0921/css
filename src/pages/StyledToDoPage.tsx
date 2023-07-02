9dd23e0a99cb9fa4b80cbdde72f829e07ae47f4a
suhyeon <suhyeon3484@naver.com>
2023-07-03 01:29:32 +0900

    feat: todo templete

---
+++ src/pages/StyledToDoPage.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import DeleteIcon from '@mui/icons-material/Delete';
+import LottieFile from '../components/LottieFile';
+
+export default function StyledToDoPage() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [input, setInput] = useState<string>('');
+
+  const addTodo = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    if (input !== '') {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  const deleteTodo = (index: number): void => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <span>ToDo List</span>
+      <form onSubmit={addTodo}>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type='submit'>Add</button>
+      </form>
+      {todos.map((todo, index) => (
+        <>
+          <span>{todo}</span>
+          <button onClick={() => deleteTodo(index)}>
+            <DeleteIcon />
+          </button>
+        </>
+      ))}
+    </>
+  );
+}
